5912ea64623b1500f174aa99f3513dd2c42fc4fa
Mohammed R Abbas <mohammed.r.abbas@pwc.com>
2023-01-09 03:29:51 +0530

    End of Section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -4,11 +4,24 @@ import EventDashboard from '../../features/events/eventDashboard/EventDashboard'
 import NavBar from '../../features/nav/NavBar';
 export default function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event){
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
+
   return (
     <>
-      <NavBar setFormOpen={setFormOpen}/>
+      <NavBar setFormOpen={handleCreateFormOpen}/>
       <Container className='main'>
-        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+        <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen} selectEvent={handleSelectEvent} selectedEvent={selectedEvent}/>
       </Container>
       {/* <button className='ui icon red button'> 
       <i className='user icon'/> CSS Button
--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./EventListAttendee";
 
-export default function EventListItem({event}){
+export default function EventListItem({event,selectEvent, deleteEvent}){
     return(
         <Segment.Group>
             <Segment>
@@ -35,7 +35,8 @@ export default function EventListItem({event}){
                 <div>
                      {event.description}
                 </div>
-                <Button color='teal' floated='right' content='View' />
+                <Button onClick={() => deleteEvent(event.id)} color='red' floated='right' content='Delete' />
+                <Button onClick={() => selectEvent(event)} color='teal' floated='right' content='View' />
             </Segment>
         </Segment.Group> 
     )
